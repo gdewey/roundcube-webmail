176bbfbd4eb9eaad594419272293c7deddc1b36a
Guillermo Dewey <gd@metro.mx>
2026-03-15 15:35:12 -0600

    Add ManageSieve configuration to custom.inc.php for Stalwart Mail Server

--- config/custom.inc.php
+++ config/custom.inc.php
@@ -22,6 +22,16 @@ $config['draft_autosave'] = 60;
 $config['preview_pane'] = true;
 $config['check_all_folders'] = false;
 
+// --- ManageSieve (Stalwart Mail Server) ---
+$config['managesieve_port'] = 4190;
+$config['managesieve_host'] = 'tls://%h';
+$config['managesieve_auth_type'] = null;
+$config['managesieve_usetls'] = true;
+$config['managesieve_default'] = '/var/www/html/plugins/managesieve/defaut.sieve';
+$config['managesieve_vacation'] = 1;
+$config['managesieve_vacation_addresses_init'] = true;
+$config['managesieve_raw_editor'] = true;
+
 // --- Security ---
 $config['ip_check'] = true;
 $config['useragent'] = 'Roundcube Webmail';
